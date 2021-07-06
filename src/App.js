4b5e7b988c51cfca13578d45a07c8169a5129d33
sea-kai <76773825+sea-kai@users.noreply.github.com>
2021-07-06 23:11:37 +0900

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useCallback, useMemo } from "react";
 import { ChildArea } from "./ChileArea";
 import "./styles.css";
 
@@ -7,17 +7,19 @@ export default function App() {
   const [open, setOpen] = useState(false);
 
   const onChangeText = (e) => setText(e.target.value);
-  const onClickOpen = () => {
-    setOpen(!open);
-  };
+  const onClickOpen = () => setOpen(!open);
 
+  const onClickClose = useCallback(() => setOpen(false), [setOpen]);
+
+  const temp = useMemo(() => 1 + 3, []);
+  console.log(temp);
   return (
     <div className="App">
       <input value={text} onChange={onChangeText} />
       <br />
       <br />
       <button onClick={onClickOpen}>表示</button>
-      <ChildArea open={open} />
+      <ChildArea open={open} onClickClose={onClickClose} />
     </div>
   );
 }
